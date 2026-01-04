257afa7a57a5efe38cdf499d797f5b706a8cda00
mr-papu-jpg <angstart.1109@gmail.com>
2026-01-04 12:39:58 -0400

    feat: Create list with tuples inside

---
+++ registros-list-tuple.py
@@ -0,0 +1,13 @@
+medallas=[
+    ("Fire Paint", "Madrir"),
+    ("Water Fall", "Cartagena"),
+    ("Rock Down","Caracas"),
+    ("Hallow darkness", "San Cristobal")
+]
+
+print(medallas[1][1])
+print(medallas[0][1])
+print(medallas[3][0])
+
+medallas.append(("Sky blue", "Hong kong"))
+print(medallas[4][0])
